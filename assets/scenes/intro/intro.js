7c2bf6e8b566362e87bd2e8038558def412a18c6
Vengarioth <home@deviru.de>
2012-06-24 02:29:40 +0200

    aqergsrthj

--- assets/scenes/intro/intro.js
+++ assets/scenes/intro/intro.js
@@ -1,8 +1,49 @@
 function(sender, localization) {
 	
-	console.log(sender);
+	var backgrounds = [];
+	
+	var backgroundName = 'background';
+	var maxBackgrounds = 9;
+	
+	renderTaskID = null;
+	
+	for (var i = 0; i < maxBackgrounds; i++) {
+		backgrounds.push({
+			filename: Lyria.Resource.name([sender, backgroundName + i + '.png'].join('/'), 'image')
+		});
+	}
+	
+	
+	
+	function onSceneActive() {
+		(function() {
+			var viewport = new Scene_RenderTarget_Viewport($('#'), window.canvasEngine.sceneGraph);
+			renderTaskID = window.canvasEngine.loop.addTask(function(loop) {
+		    	viewport.render(loop);
+		    }, 33);
+			
+		    var ShipSprite = new Scene_Asset_Sprite();
+		    ShipSprite.load(Lyria.Resource.name('spazeship.png', 'image'), function(ShipSprite) {
+		    	for(var i = 0; i < 10; i++) {
+		        	var obj = new Game_CrashingShip(ShipSprite);
+		        	window.canvasEngine.sceneGraph.add(obj);
+		        }
+		    });
+		}).delay(100);
+	}
+	
+	function onSceneDeactived() {
+		loop.removeTask(renderTaskID);
+	}
+	
+	function update() {
+		
+	}
 	
 	return {
-		test: "Hallo"
+		backgrounds: backgrounds,
+		update: update,
+		onSceneActive: onSceneActive,
+		onSceneDeactived: onSceneDeactived,
 	}
 }
---
+++ js/Game/CrashingShip.js
@@ -0,0 +1,12 @@
+/**
+ *	@author Bloody
+ */
+var Game_CrashingShip = new Class({
+	Extends: Scene_Node_Drawable,
+	
+	
+	initialize : function(sprite) {
+		this.sprite = sprite;
+	},
+
+});
