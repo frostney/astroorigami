67c2974042d78b98d0f45306cbecfc33abab3823
Johannes Stein <johannesstein@freeze-dev.de>
2012-06-24 02:29:13 +0200

    intro div + plus other puzzle1-related stuff

--- assets/scenes/puzzle1/puzzle1.js
+++ assets/scenes/puzzle1/puzzle1.js
@@ -1,6 +1,13 @@
 function(sender, localization) {
 	
+	var backgroundName = 'background';
+	var maxBackgrounds = 7;
+	
 	var backgrounds = [];
+	
+	for (var i = 0; i < maxBackgrounds; i++) {
+		backgrounds.push(Lyria.Resource.name([sender, backgroundName + i + '.png'].join('/'), 'image'));
+	}
 	
 	return {
 		backgrounds: backgrounds
